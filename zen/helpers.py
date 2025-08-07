29ad02875322e7acd0d0053772b798705018c5b7
haseeb <haseeeb8j@gmail.com>
2025-08-07 15:24:58 +0500

    added log/ check

--- zen/helpers.py
+++ zen/helpers.py
@@ -1,3 +1,52 @@
+from pathlib import Path
+import os
 
-def ask(msg):
-    pass
+def ask_user(msg: str, options: list[str]) -> str:
+
+    while True:
+        choice = input(f"{msg} {tuple(options)} : ")
+
+        if choice in options:
+            return choice
+        
+        print("Incorrect option!")
+
+
+
+def check_log_dir() -> bool:
+
+    parent = Path(__file__).resolve().parents[1]
+
+    log_dir = parent / "logs"
+
+
+    if not log_dir.exists():
+        choice = helpers.ask_user(
+            f"{log_dir} does not exist. Do you want to create it?",
+            ['y', 'n']
+        )
+
+        if choice == 'y':
+            os.mkdir(log_dir)
+            return True
+        else:
+            return False
+
+
+    if not log_dir.is_dir():
+        choice = helpers.ask_user(
+            f"{log_dir} is a file. Do you want to remove it and create a directory?",
+            ['y', 'n']
+        )
+
+        if choice == 'y':
+            os.remove(log_dir)
+            os.mkdir(log_dir)
+            return True
+        else:
+            return False
+        
+
+    
+
+
