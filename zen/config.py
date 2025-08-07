cbd3bd6d56d4db09237c5329ef09b72876c87fa5
haseeb <haseeeb8j@gmail.com>
2025-08-07 20:35:44 +0500

    imporved config

--- zen/config.py
+++ zen/config.py
@@ -1,7 +1,29 @@
 from pathlib import Path
+import platform, os
 
-PARENT_DIR = Path(__file__).resolve().parents[1]
-ZEN_DIR = Path(__file__).resolve().parent
-LOG_DIR = PARENT_DIR / "logs"
+def configure():
+    
+    print(f"Did not find {ZEN_DIR}. Initializing...")
+    os.mkdir(ZEN_DIR)
+    os.mkdir(LOG_DIR)
 
+    settings = {
+        "name" : "User"
+    }
+
+    with open(SETTINGS_FILE) as file:
+        json.dump(settings, file)
+
+
+if platform.system == "Windows":
+    ZEN_DIR = Path(os.environ("appdata")) / "zen"
+else:
+    ZEN_DIR = Path(os.path.expanduser("~/.config")) / "zen"
+
+LOG_DIR = ZEN_DIR / "logs"
 SETTINGS_FILE = ZEN_DIR / "settings.json"
+
+if not ZEN_DIR.exists():
+    configure()
+    
+
